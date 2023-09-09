0446ffd118c9afd226b08cdd824826a12eb6a75f
Bram Alkema <ynse@Brams-MacBook-Air.local>
2023-09-10 00:01:20 +0200

    Your message here

---
+++ app/services/game_logic.py
@@ -0,0 +1,10 @@
+import time
+
+def run():
+    # Your main game logic or function calls go here
+    try:
+        while True:
+            print("Mastodon Game Running...")
+            time.sleep(10)  # Example delay
+    except Exception as e:
+        print(f"Error: {e}")
---
+++ main.py
@@ -0,0 +1,15 @@
+import time
+from app.services import game_logic
+
+def main():
+    # Initialization or setup code
+    print("Mastodon Game Initializing...")
+
+    # Hand over to the primary game logic
+    game_logic.run()
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nMastodon Game Stopped.")
