672e039283ea62a05aec59e695b7d2184ea5e3fa
Bram Alkema <bram@drog.group>
2023-09-12 00:35:25 +0200

    new file:   app/services/bot_service.py 	new file:   app/services/event_service.py 	modified:   app/services/game_logic.py 	new file:   app/services/user_service.py

--- app/services/game_logic.py
+++ app/services/game_logic.py
@@ -2,12 +2,29 @@
 # and it coordinates the various services and models to create the game experience.
 
 import time
+from app.services import mastodon_api, openai_api, event_service, user_service, bot_service
 
-def run():
-    # Your main game logic or function calls go here
-    try:
-        while True:
-            print("Mastodon Game Running...")
-            time.sleep(10)  # Example delay
-    except Exception as e:
-        print(f"Error: {e}")
+class MastodonGame:
+    def __init__(self):
+        # Initialization of game entities, services, etc.
+        pass
+
+    def run(self):
+        """Primary game loop."""
+        try:
+            while True:
+                interactions = mastodon_api.check_user_interactions()
+                for interaction in interactions:
+                    content = openai_api.generate_content(interaction)
+                    mastodon_api.post_response(content)
+                    event_service.trigger_events(interaction)
+                    user_service.update_users()
+                    bot_service.update_bots()
+                time.sleep(10)  # Sleep for a while before checking for new interactions
+        except KeyboardInterrupt:
+            print("Game terminated by user.")
+
+# To run the game
+if __name__ == "__main__":
+    game = MastodonGame()
+    game.run()
